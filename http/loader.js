297b45c196fe8727198aad42c4a64d4cfcac746c
Rui Gomes <yruigomes99@gmail.com>
2024-08-24 13:44:45 +0100

    fix

--- http/loader.js
+++ http/loader.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 })
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello World' })
+    res.status(200).json({ message: 'Hello World' })
 })
 
 app.use(express.json())
